c96497f929e1d5bc95e7f4a7ee23a2f849a67759
Gustavo-Almeida-M <gustavoam916@gmail.com>
2024-07-30 21:19:03 -0300

    commit test

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
 
+
+
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
